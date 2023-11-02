6856b65f86ba9c8fe39214f4a0e8e389d8de5086
Hugodesousa <hugodesousa.16@gmail.com>
2023-11-02 13:23:04 -0300

    rota para listar usuarios

--- backend/src/Controllers/users.controller.ts
+++ backend/src/Controllers/users.controller.ts
@@ -9,34 +9,38 @@ class UsersController extends Controller {
 
   constructor(req: Request, res: Response, next: NextFunction) {
     super(req, res, next)
-    this.usersService = new UsersService();
+    this.usersService = new UsersService(res);
   }
 
   public async findAllUsers() {
-    const sql = `SELECT
-    user_nome,
-    user_email,
-    u.data_cadastro,
-    ut.tipo_usuario,
-    e.end_completo,
-    c.ddd || c.telefone as telefone
+    const sql = `
+    SELECT 
+      u.pk_user_id,
+      u.user_nome,
+      u.user_email,
+      u.data_cadastro,
+      ut.tipo_usuario,
+      e.end_completo,
+      c.ddd || c.telefone as telefone
     FROM usuarios as u
-    INNER JOIN usuarios_tipo as ut on u.fk_user_tipo_id = ut.pk_user_tipo_id
-    INNER JOIN endereco as e on e.pk_endereco_id = u.fk_endereco_id
-    inner join contatos as c on u.pk_user_id = c.fk_user_id;`
-
+    INNER JOIN usuarios_tipo as ut ON u.fk_user_tipo_id = ut.pk_user_tipo_id
+    INNER JOIN endereco as e ON e.pk_endereco_id = u.fk_endereco_id
+    LEFT JOIN contatos as c ON u.pk_user_id = c.fk_user_id;
+`
     try {
       db.all(sql, (selectErr, rows) => {
         if (selectErr) {
-          console.error('Erro ao executar a consulta SELECT', selectErr.message);
-          return this.res.status(500).send('Erro na consulta.');
+          console.error('Erro ao executar a consulta --->', selectErr.message);
+          return this.res.status(500).send(selectErr.message);
         } else {
           return this.res.status(200).json(rows);
         }
       });
-      //const allUsers: void | IUsers[] = await this.usersService.findAllUsers();
-      // return this.res.status(200).json(allUsers);
-      // return this.res.status(200).json([{nome:"hugo"},{nome:"teste"}]);
+
+
+      // const users = this.usersService.findAllUsers()
+      // return this.res.status(200).json(users);
+      
     } catch (error) {
       this.next(error)
     }
